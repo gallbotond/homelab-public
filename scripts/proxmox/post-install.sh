50eb78eb9377669c969beecfda4f5876b99db6ec
gallbotond <gallbotondofc@gmail.com>
2026-03-26 23:37:04 +0200

    refactor post-install script to include nag-buster and dependencies

--- scripts/proxmox/post-install.sh
+++ scripts/proxmox/post-install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# nag-buster
+bash <(curl -s https://raw.githubusercontent.com/foundObjects/pve-nag-buster/refs/heads/master/install.sh)
+
+# post install script dependencies
+apt-get install git -y
+
+# post install script
 git clone https://github.com/Lalatenduswain/ProxmoxVE-Post-Install-Script.git
 cd ProxmoxVE-Post-Install-Script
 
