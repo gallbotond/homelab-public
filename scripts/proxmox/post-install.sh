604c6e330f0c8dc0ee2646ebf5ae428e050d99cf
gallbotond <gallbotondofc@gmail.com>
2026-04-11 02:20:02 +0300

    fix: improve error handling in post-install script

--- scripts/proxmox/post-install.sh
+++ scripts/proxmox/post-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # nag-buster
 bash <(curl -s https://raw.githubusercontent.com/foundObjects/pve-nag-buster/refs/heads/master/install.sh)
 
@@ -8,11 +10,22 @@ apt-get update
 apt-get install git -y
 
 # post install script
-git clone https://github.com/Lalatenduswain/ProxmoxVE-Post-Install-Script.git
-cd ProxmoxVE-Post-Install-Script
+git clone https://github.com/Lalatenduswain/ProxmoxVE-Post-Install-Script.git &&
+	cd ProxmoxVE-Post-Install-Script || {
+	echo "Clone/cd failed"
+	exit 1
+}
 
 chmod +x post-pve-install.sh
 # ./post-pve-install.sh
 
 # set up tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
+
+# set up zoxide
+apt-get install zoxide -y
+echo 'eval "$(zoxide init bash)"' >>~/.bashrc
+# source ~/.bashrc
+
+# install remaining tools
+apt-get install btop -y
